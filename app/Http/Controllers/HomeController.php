bd86dfd0de628239e642f0815f890507c59108bb
said elhadi <saidelhadi0000@gmail.com>
2024-02-09 12:43:20 +0100

    second commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view("welcome");
+        $data=1;
+        return view("welcome",["data"=>$data]);
     }
 }
